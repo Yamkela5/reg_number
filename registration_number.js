1dbc0331662b5df9bc149643016103f844e42b97
Yamkela <owamjames1@gmail.com>
2017-03-22 12:25:44 +0200

    adding new files

--- registration_number.js
+++ registration_number.js
@@ -2,7 +2,7 @@
 var txtbox = document.querySelector('.textbox');
 var plte = document.querySelector('.plates');
 var add = document.querySelector('.add');
-
+var dropbox =document.querySelector('#dropbox');
 
 add.addEventListener("click",function() {
   if (txtbox.value.length > 0) {
@@ -12,3 +12,22 @@ add.addEventListener("click",function() {
     txtbox.value = '';
   }
 });
+dropbox.addEventListener('change', function(e) {
+  var places = document.querySelectorAll('.numplate');
+  console.log(places);
+
+  for(var i =0; i<places.length; i++){
+    if (e.target.value=== 'All') {
+
+      places[i].style.display= "block";
+    }
+      else if (places[i].innerHTML.startsWith(e.target.value))
+      {
+        places[i].style.display='block';
+      }
+      else{
+        places[i].style.display='none';
+      }
+    }
+
+})
